d66f11149d59a2f52f3249e09b2838b984195aba
johnpaulonza <johnpaul_onza@yahoo.com>
2022-05-01 23:14:01 -0700

    Fixed test

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,2 +1,6 @@
 public class SkillDemo1{
+
+    public static int avgOf3(int x, int y, int z){
+        return (x + y + z)/3;
+    }
 }
---
+++ SkillDemo1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Test {
+    @Test
+    public void avgOf3(){
+        assertEquals(2, SkillDemo1.avgOf3(1, 2, 3));
+    }
+}
